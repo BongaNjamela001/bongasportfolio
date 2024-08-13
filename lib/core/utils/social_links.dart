adbedc8fb066e9c1d3a826349b780c5f18ea4f89
Bonga Njamela <75367761+BongaNjamela001@users.noreply.github.com>
2024-08-13 14:26:39 +0200

    Delete lib directory

--- lib/core/utils/social_links.dart
+++
@@ -1,7 +0,0 @@
-abstract class SocialLinks {
-  static const String resume =
-      "https://drive.google.com/file/d/1mO8gx34HWs5g3EwrnPRu5EiQR6e4OUSZ/view?usp=sharing";
-  static const String github = "https://github.com/BongaNjamela001";
-  static const String linkedin =
-      "https://www.linkedin.com/in/bonga-njamela-982873200/";
-}
